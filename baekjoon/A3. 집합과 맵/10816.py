9f7d2332d04a605bae29997f110f94c2a55e2f56
Justin Oh <94172351+ohiju@users.noreply.github.com>
2023-01-23 01:04:16 +0900

    Add files via upload

---
+++ baekjoon/A3. 집합과 맵/10816.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+input = sys.stdin.readline
+
+# 여기부터 백준 코드 입력
+
+from sys import stdin
+_ = stdin.readline()
+N = sorted(map(int,stdin.readline().split()))
+_ = stdin.readline()
+M = map(int,stdin.readline().split())
+
+def binary(n, N, start, end):
+    if start > end:
+        return 0
+    m = (start+end)//2
+    if n == N[m]:
+        return N[start:end+1].count(n)
+    elif n < N[m]:
+        return binary(n, N, start, m-1)
+    else:
+        return binary(n, N, m+1, end)
+
+n_dic = {}
+for n in N:
+    start = 0
+    end = len(N) - 1
+    if n not in n_dic:
+        n_dic[n] = binary(n, N, start, end)
+
+print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in M ))
