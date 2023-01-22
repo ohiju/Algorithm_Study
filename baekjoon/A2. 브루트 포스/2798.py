c8fad6db65fc6185634a4dadf0d9d2614f74a850
Justin Oh <94172351+ohiju@users.noreply.github.com>
2023-01-23 01:03:54 +0900

    Add files via upload

---
+++ baekjoon/A2. 브루트 포스/2798.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+input = sys.stdin.readline
+
+# 여기부터 백준 코드 입력
+
+N,M = map(int,input().split())
+arr = list(map(int,input().split()))
+result = 0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1, N):
+            if arr[i] + arr[j] + arr[k] > M:
+                continue
+            else:
+                result = max(result, arr[i]+arr[j]+arr[k])
+print(result)
